e8a3ac6b2f5f0e78156feaf2b45ea8e3a53213ff
Manuel Cole <Manuelcole35@gmail.com>
2025-11-20 10:58:29 -0500

    Clean up static: move images, remove products folder, rename Static to static

--- Nonecom/settings.py
+++ Nonecom/settings.py
@@ -123,7 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFLIES_DIRS =['static/']
+
+STATICFILES_DIRS = [
+    BASE_DIR / "Nonecom" / "Static",
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
